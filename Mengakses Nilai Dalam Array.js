7c550086b872d58d7d88ef12c20bd2dbe25b9428
falend <55618978+falend@users.noreply.github.com>
2020-04-03 22:51:18 +0800

    array

---
+++ Mengakses Nilai Dalam Array.js
@@ -0,0 +1,15 @@
+var hasil = '';
+for(var i = balikString.length - 1; i >= 0; i--) {
+   hasil = hasil + balikString[i];
+}
+
+console.log(hasil);
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+for (var j = arr.length -1; j >= 0; j--){
+  console.log(arr[j]);
+}
+for (var i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+}
